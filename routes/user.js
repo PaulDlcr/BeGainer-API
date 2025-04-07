71e880324a8276421c6e4cc9587b0484f52d6f96
PaulDlcr <delacrepaul@gmail.com>
2025-04-07 16:02:40 +0200

    Fix user

--- routes/user.js
+++ routes/user.js
@@ -83,7 +83,7 @@ router.get('/:id', async (req, res) => {
  */
 router.put('/:id', async (req, res) => {
     const { id } = req.params;
-    const { email, name } = req.body;
+    const { email } = req.body;
   
     try {
       const result = await pool.query(
